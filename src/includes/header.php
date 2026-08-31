c376e30350db24242c2202169fd8ff1296b6377a
ElliotPa1m <elliot.palm@hotmail.com>
2026-08-31 13:30:56 +0200

    created folders for all the files and renamed them to index for a cleaner URL, also fixed the links in the login/logout/register pages to match the folder layout, added DirectoryIndex to htaccess and slightly restructured the layout of the header for easier css later on

--- src/includes/header.php
+++ src/includes/header.php
@@ -5,13 +5,13 @@ if (session_status() === PHP_SESSION_NONE) {
 ?>
 
 <nav>
-    <a href="index.php">Home</a>
+    <a href="/">Home</a>
 
     <?php if (isset($_SESSION['user_id'])): ?>
-        <span>Welcome, <?= htmlspecialchars($_SESSION['first_name']) ?>!</span>
-        <a href="logout.php">Log out</a>
+        <span><?= htmlspecialchars($_SESSION['first_name']) ?></span>
+        <a href="/logout/">Log out</a>
     <?php else: ?>
-        <a href="login.php">Log in</a>
-        <a href="register.php">Create account</a>
+        <a href="/login/">Log in</a>
+        <a href="/register/">Create account</a>
     <?php endif; ?>
 </nav>
